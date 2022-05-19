7598f9097d6bde2cf286baaf3f30bf479b5149a0
Carli9510 <99306256+Carli9510@users.noreply.github.com>
2022-05-19 08:48:24 -0400

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,5 @@
+api_hash = "74e87e9ae1d822d0e7872e080ed689a2"
+api_id = 16295046
+bot_token = "5346080848:AAFk3_Uw08uiT2Ic2GRaWgxa6cOnxLGo6gE"
+authorized_users = [Charls9510]
+admin_user = 'Charls9510'
